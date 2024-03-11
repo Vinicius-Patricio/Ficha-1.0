130777e586a09ad7d2e78afacba2a45b2c6a8bce
Vinicius Patricio <viniciuspatricio.33@gmail.com>
2024-03-11 11:36:54 -0300

    Removi a classe cria_ficha.php, vi que no momento nao fazia tanto sentido criar um classe sobre isso, futuramente quando estiver mais  familiarizado posso adicionar novamente.

--- Fast Ficha/restrito/cria_ficha_script.php
+++ Fast Ficha/restrito/cria_ficha_script.php
@@ -1,11 +1,49 @@
 <?php
-include('classe_ficha.php');
-session_start();
+    require('conexao.php');
+    session_start();
+    $id_aluno = $_GET['id'] ?? null;
 
-$gerador = new FichaTreinoGenerator($conn);
+    if ($id_aluno) {
 
-$fichaTreino = $gerador->gerarFichaTreino($_GET['id']);
+        $sql_exp = "SELECT exp_treino FROM aluno WHERE id_aluno = ?";
+        $stmt_exp = $conn->prepare($sql_exp);
+        $stmt_exp->bind_param("i",$id_aluno);
+        $stmt_exp->execute();
+        $result_exp = $stmt_exp->get_result();
+        $exp = $result_exp->fetch_assoc()['exp_treino'];
+        $stmt_exp->close();
 
-echo"<pre>";
-print_r($fichaTreino);
-echo"</pre>";
+     
+        $sql_series = "SELECT seriestreino.grupo_muscular, seriestreino.rep, seriestreino.series, aluno.exp_treino, aluno.id_aluno 
+                        FROM seriestreino
+                        INNER JOIN exptreino ON seriestreino.exp_treino = exptreino.id
+                        INNER JOIN treinos ON exptreino.id_grupo = treinos.id_grupo
+                        INNER JOIN aluno ON aluno.id_aluno = treinos.id_aluno
+                        WHERE aluno.id_aluno = $id_aluno";
+
+        $stmt_series = $conn->prepare($sql_series);
+        $stmt_series->bind_param("i", $id_aluno);
+        $stmt_series->execute();
+        $resultado_series = $stmt_series->get_result();
+        $series = $resultado_series->fetch_all(MYSQLI_ASSOC);
+        $stmt_series->close();
+
+
+        $sql_exercicios = "SELECT e.nome AS nome_exercicio, gm.nome AS nome_grupo_muscular 
+                            FROM exercicios e 
+                            INNER JOIN exerciciosgruposmusculares egm ON e.id = egm.exercicio_id
+                            INNER JOIN gruposmusculares gm ON egm.grupo_muscular_id = gm.id";
+
+        $resultado_exercicios = $conn->query($sql_exercicios);
+        $exercicios = $resultado_exercicios->fetch_all(MYSQLI_ASSOC);
+
+        if ($series && $exercicios) {
+            echo "<pre>";
+            print_r($exercicios);
+            echo "</pre>";
+        } else {
+            echo "Ocorreu um erro ao recuperar os dados.";
+        }
+    } else {
+        echo "ID do aluno não fornecido.";
+    }
